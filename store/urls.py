9e96c9596a36932640aa1025bd4587f1a0fd6c87
omar <omar3166435@gmail.com>
2025-01-05 18:50:45 +0300

    setup the routing using Router in DRF

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 router = SimpleRouter()
 router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
 urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection
+from .models import OrderItem, Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models.aggregates import Count
 
@@ -12,11 +12,13 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
 
-    def destroy(self,request,pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
-            return Response({'error': 'Cannot Delete product because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Cannot Delete product because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED) 
+        return super().destroy(request, *args, **kwargs)
+
+
     
 
 
@@ -24,11 +26,12 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).order_by('id')
     serializer_class = CollectionSerializer
 
-    def destroy(self,request,pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Cannot delete collection because it has products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+   
 
     
 
