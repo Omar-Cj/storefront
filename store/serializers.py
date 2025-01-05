0c4c284958363cf59edc3cdd9a08a9e8af411007
omar <omar3166435@gmail.com>
2025-01-05 18:12:47 +0300

    defined basic api for managing products and collections

--- store/serializers.py
+++ store/serializers.py
@@ -3,17 +3,23 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title','products_count']
+
+    products_count = serializers.IntegerField()     
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'discount', 'collection']
+   
     discount = serializers.SerializerMethodField(method_name='calculate_discount')
-    collection = serializers.StringRelatedField()
+   #  collection = serializers.StringRelatedField()
 
     def calculate_discount(self, product: Product):
         return product.unit_price * Decimal(0.5) 
+    
+   
