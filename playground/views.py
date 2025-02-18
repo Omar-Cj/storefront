1c34facb0d75a982f444e6300a23cb8a7129440a
omar <omar3166435@gmail.com>
2025-02-18 20:08:16 +0530

    Testing sending emails from production environment

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,13 @@
-from django.core.cache import cache
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
-import logging
-import requests
-
-logger = logging.getLogger(__name__) 
+from templated_mail.mail import BaseEmailMessage
 
 class HelloView(APIView):
     def get(self, request):
         try:
-            logger.info('Calling httpbin')
-            response = requests.get('https://httpbin.org/delay/2')
-            logger.info('Received the response')
-            data = response.json()
-        except requests.ConnectionError:
-            logger.critical('httpbin is offline')
-        return render(request, 'index.html', {'name': 'Mosh'})
+            message = BaseEmailMessage(template_name='emails/hello.html')
+            message.send(['omar3166435@gmail.com'])
+        except BadHeaderError:
+            pass
+        return render(request, 'index.html')  
